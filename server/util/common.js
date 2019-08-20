de13e921945da849865603f7420aeb8019822371
Warro <olli.warro@gmail.com>
2019-08-20 16:30:02 +0300

    Added student service

--- server/util/common.js
+++ server/util/common.js
@@ -4,4 +4,6 @@ module.exports = {
   ...common,
   DB_URL: process.env.DB_URL,
   PORT: process.env.PORT || 8000,
+  STUDENT_API_URL: process.env.STUDENT_API_URL,
+  STUDENT_API_TOKEN: process.env.STUDENT_API_TOKEN,
 }
