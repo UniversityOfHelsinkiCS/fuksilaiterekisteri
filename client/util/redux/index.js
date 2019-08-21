1acb023094971f2b44dd21f7a6ecb8bdbd21d635
Kousa <jami.kousa@helsinki.fi>
2019-08-21 14:44:36 +0300

    student search for distributors

--- client/util/redux/index.js
+++ client/util/redux/index.js
@@ -2,8 +2,14 @@ import { combineReducers } from 'redux'
 
 import messages from './messageReducer'
 import user from './userReducer'
+import student from './studentReducer'
+import deviceClaim from './deviceClaimReducer'
+import deviceRequest from './deviceRequestReducer'
 
 export default combineReducers({
   messages,
   user,
+  student,
+  deviceClaim,
+  deviceRequest,
 })
--- server/controllers/studentController.js
+++ server/controllers/studentController.js
@@ -1,5 +1,18 @@
-const getStudent = (req, res) => {
-  res.send({ number: req.params.studentNumber })
+const db = require('@models')
+
+const getStudent = async (req, res) => {
+  const { studentNumber } = req.params
+
+  const student = await db.user.findOne({ where: { studentNumber } })
+  if (!student) return res.sendStatus(404)
+
+  const response = {
+    studentNumber: student.studentNumber,
+    name: student.name,
+    dateOfBirth: student.dateOfBirth,
+  }
+
+  return res.send(response)
 }
 
 
