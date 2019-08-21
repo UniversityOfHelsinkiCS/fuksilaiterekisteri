179d58eb6071c402847d91fd8aff341472238cfb
Kousa <jami.kousa@helsinki.fi>
2019-08-21 13:20:50 +0300

    zimble roudring

--- client/components/Router.js
+++ client/components/Router.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 import MainPage from 'Components/MainPage'
 import AdminPage from 'Components/AdminPage'
@@ -9,16 +10,27 @@ import StudentPage from 'Components/StudentPage'
 import UnauthorizedPage from 'Components/UnauthorizedPage'
 import NotFoundPage from 'Components/NotFoundPage'
 
-export default () => (
-  <div className="content">
-    <Switch>
-      <Route exact path="/" component={MainPage} />
-      <Route exact path="/admin" component={AdminPage} />
-      <Route exact path="/distributor" component={DistributorPage} />
-      <Route exact path="/staff" component={StaffPage} />
-      <Route exact path="/student" component={StudentPage} />
-      <Route exact path="/unauthorized" component={UnauthorizedPage} />
-      <Route path="*" component={NotFoundPage} />
-    </Switch>
-  </div>
-)
+const Router = ({ user }) => {
+  if (!user) return <div> Loading </div>
+  // Filter router based on user
+  console.log('Router, user', user)
+  return (
+    <div className="content">
+      <Switch>
+        <Route exact path="/" component={MainPage} />
+        <Route exact path="/admin" component={AdminPage} />
+        <Route exact path="/distributor" component={DistributorPage} />
+        <Route exact path="/staff" component={StaffPage} />
+        <Route exact path="/student" component={StudentPage} />
+        <Route exact path="/unauthorized" component={UnauthorizedPage} />
+        <Route path="*" component={NotFoundPage} />
+      </Switch>
+    </div>
+  )
+}
+
+const mapStateToProps = ({ user }) => ({
+  user: user.data,
+})
+
+export default connect(mapStateToProps)(Router)
