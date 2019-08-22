b50e6bba420c48b3da8042dee79f2fbb4b98ae49
Jere Helén <jere.helen@helsinki.fi>
2019-08-22 12:52:47 +0300

    Task page betterment

---
+++ client/components/StudentPage/ClaimingInfo.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { Segment } from 'semantic-ui-react'
+
+const ClaimingInfo = () => <Segment>You can has a läppäri. Please message us multiple times for further info.</Segment>
+export default ClaimingInfo
