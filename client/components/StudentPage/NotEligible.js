04e41633c369b1d40d9305bcee2a96b3ee56af45
Tero Tapio <tero.tapio@helsinki.fi>
2019-08-22 10:00:43 +0300

    adds mention bachelor only, non-exchange only

--- client/components/StudentPage/NotEligible.js
+++ client/components/StudentPage/NotEligible.js
@@ -8,18 +8,28 @@ const NotEligible = () => (
         <b>Valitettavasti et ole oikeutettu fuksilaitteeseen.</b>
       </p>
       <p>
-        Laitteet lainataan vain tänä lukukautena virallisesti ensimmäistä kertaa läsnäoleville matemaattis-luonnontieteellisen tiedekunnan opiskelijoille. Tähän sääntöön on harvinaisia poikkeuksia,
-        sillä esimerkiksi ennen fuksilaitteiden lainausmahdollisuutta läsnäolleet (esim. ennen 2008 tietojenkäsittelytiedettä opiskelleet) ovat nyt oikeutettuja laitteeseen. Jos kuulut tällaiseen
-        ryhmään, mutta näet tämän sivun, ota yhteyttä opinto-ohjelmasi fuksilaitevastaavaan.
+        Laitteet lainataan vain tänä lukukautena virallisesti ensimmäistä kertaa
+        läsnäoleville matemaattis-luonnontieteellisen tiedekunnan
+        kandiopiskelijoille, poislukien vaihto-oppilaat. Tähän sääntöön on
+        harvinaisia poikkeuksia, sillä esimerkiksi ennen fuksilaitteiden
+        lainausmahdollisuutta läsnäolleet (esim. ennen 2008
+        tietojenkäsittelytiedettä opiskelleet) ovat nyt oikeutettuja
+        laitteeseen. Jos kuulut tällaiseen ryhmään, mutta näet tämän sivun, ota
+        yhteyttä opinto-ohjelmasi fuksilaitevastaavaan.
       </p>
       <hr />
       <p>
         <b>Unfortunately you are not eligible for the fresher device.</b>
       </p>
       <p>
-        The devices are only available for students who are currently officially present for the first time for studies in the Faculty of Science. There are rare exceptions to this rule, such as
-        people who have previously been present for studies, but before the lending of devices began (e.g. present before 2008 for Computer Science). If you consider yourself eligible, but are seeing
-        this page, please contact the person responsible for fresher devices in your study programme to request an exception.
+        The devices are only available for students who are currently officially
+        present for the first time for Bachelor level studies in the Faculty of
+        Science (excluding exchange students). There are rare exceptions to this
+        rule, such as people who have previously been present for studies, but
+        before the lending of devices began (e.g. present before 2008 for
+        Computer Science). If you consider yourself eligible, but are seeing
+        this page, please contact the person responsible for fresher devices in
+        your study programme to request an exception.
       </p>
     </div>
   </Segment>
