f43b399880b9f21f5663f4f543576d3887e0d91b
Warro <olli.warro@gmail.com>
2019-08-26 15:48:01 +0300

    Added header to staff page

--- client/components/StaffPage/index.js
+++ client/components/StaffPage/index.js
@@ -1,11 +1,13 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { Header, Segment } from 'semantic-ui-react'
 import { getStudentsAction } from '../../util/redux/studentReducer'
 import StudentTable from './StudentTable'
 
 export default () => {
   const dispatch = useDispatch()
   const students = useSelector(state => state.student.students)
+  const user = useSelector(state => state.user.data)
 
   useEffect(() => {
     dispatch(getStudentsAction())
@@ -13,7 +15,14 @@ export default () => {
 
   return (
     <div style={{ maxWidth: '100%' }}>
-      <h1>Hei, olet työntekijä</h1>
+      <Segment>
+        <Header as='h3'>
+          Sinulla on oikeus seuraaviin opinto-ohjelmiin:
+          <ul>
+            {user.studyPrograms.map(s => <li key={s.code}>{`${s.name} (${s.code})`}</li>)}
+          </ul>
+        </Header>
+      </Segment>
       <StudentTable students={students} />
     </div>
   )
