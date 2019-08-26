e5c688362129a10a298a0bcaa16bfa461ce7bc03
Warro <olli.warro@gmail.com>
2019-08-26 10:04:39 +0300

    Fixed bug where logout button wasnt shown if logging in failed for some reason

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import { Button } from 'semantic-ui-react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { logoutUserAction } from 'Utilities/redux/userReducer'
 
 export default () => {
   const dispatch = useDispatch()
-  const user = useSelector(state => state.user)
   const handleLogout = () => {
     dispatch(logoutUserAction())
   }
@@ -13,9 +12,7 @@ export default () => {
   return (
     <div className="navbar" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
       <h1>FUKSILAITTEET</h1>
-      {user.data ? (
-        <Button style={{ position: 'absolute', right: 0, marginRight: '10px' }} icon="sign-out" onClick={handleLogout} />
-      ) : null}
+      <Button style={{ position: 'absolute', right: 0, marginRight: '10px' }} icon="sign-out" onClick={handleLogout} />
     </div>
   )
 }
