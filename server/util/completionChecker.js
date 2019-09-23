ef733293900acefed46eb1afd8963fb28a2e70ff
Tero Tapio <tero.tapio@helsinki.fi>
2019-09-23 14:12:25 +0300

    updates distribution date to 25.9

--- server/util/completionChecker.js
+++ server/util/completionChecker.js
@@ -2,14 +2,9 @@ const sendEmail = require('@util/sendEmail')
 const logger = require('@util/logger')
 
 /* eslint-disable no-irregular-whitespace */
-const text = `Onneksi olkoon, olet täyttänyt kaikki fuksilaitteen lainaehdot! Koneita voi hakea viikolla 37 (9.9.-13.9.) allaolevan aikataulun mukaisesti. Mukaan tarvitset opiskelijanumerosi sekä tavan todistaa henkilöllisyytesi(ajokortti, passi, tai henkilökortti).
+const text = `Onneksi olkoon, olet täyttänyt kaikki fuksilaitteen lainaehdot! Voit hakea koneesi Physicumin aulasta keskiviikkona (25.9) klo 9.30-11.30. Mukaan tarvitset opiskelijanumerosi sekä tavan todistaa henkilöllisyytesi(ajokortti, passi, tai henkilökortti).
 
-Congratulations, you have completed all the tasks required to borrow the fresher device. Devices are distributed on the week 37 (9th to 13th of September) following the schedule below. Be sure to bring a photo ID and your student number with you.
-
-Huone/Room G103d, Kumpula Tiedekirjasto/Science Library
-TI/TUE 10.9.2019, 9.45 — 10.15
-KE/WED 11.9.2019, 16.00 — 16.30
-TO/THU 12.9.2019, 9.45 — 10.15 ja 16.00 — 16.30
+Congratulations, you have completed all the tasks required to borrow the fresher device. Devices are distributed in Physicum's lobby on Wednesday (25th September) at 9:30-11:30. Be sure to bring a photo ID and your student number with you.
 
 Erikoistapauksissa voit olla yhteydessä suoraan jakelijaan:
 In case of special circumstances, please contact the distributor directly:
@@ -19,7 +14,7 @@ Ystävällisin terveisin, - Kind regards
 Matemaattis-luonnontieteellinen tiedekunta / Faculty of Science`
 /* eslint-enable no-irregular-whitespace */
 
-const subject = 'Fuksilaitejakelu viikolla 37 - Fresher device distribution on week 37'
+const subject = 'Fuksilaitejakelu viikolla 39 - Fresher device distribution on week 39'
 
 const completionChecker = async (user) => {
   if (user.wantsDevice && user.eligible && user.digiSkillsCompleted && user.courseRegistrationCompleted) {
