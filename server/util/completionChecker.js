d1396c47060b677831a341c553748bad7b747703
Tero Tapio <tero.tapio@helsinki.fi>
2019-10-14 12:10:04 +0300

    fixes email typo, improves email title

--- server/util/completionChecker.js
+++ server/util/completionChecker.js
@@ -7,7 +7,7 @@ const text = `Onneksi olkoon, olet täyttänyt kaikki fuksilaitteen lainaehdot! 
 Congratulations, you have completed all the tasks required to borrow the fresher device. Devices are distributed in Science Library's room G103d on Tuesday (15th October) at 16:00-17:00 and on Thursday (17th October) at 9:30-10:30. Be sure to bring a photo ID and your student number with you.
 
 HUOM! Laite pitää noutaa tai sille pitää sopia noutoaika viimeistään perjantaina 18.10.2019. Tämän jälkeen varaukset raukeavat eikä fuksiläppäriä ole enää mahdollista saada.
-NB! The device must be picked up, or the pickup must be agreed on by Friday 18th October the latest! After Friday, all reservations expire, and is no longer possible to receive a fresher device.
+NB! The device must be picked up, or the pickup must be agreed on by Friday 18th October the latest! After Friday, all reservations expire, and it is no longer possible to receive a fresher device.
 
 Jos et pääse hakemaan laitetta yo. aikoina, sovi henkilökohtainen hakuaika suoraan jakelijan kanssa sähköpostitse:
 If you aren't able to pick up the device during the above mentioned times, please contact the distributor directly to agree on the pickup:
@@ -17,7 +17,7 @@ Ystävällisin terveisin, - Kind regards
 Matemaattis-luonnontieteellinen tiedekunta / Faculty of Science`
 /* eslint-enable no-irregular-whitespace */
 
-const subject = 'Fuksilaitteen luovutus - Fresher device distribution'
+const subject = 'Fuksilaitteen luovutus viikolla 42 - Fresher device distribution on week 42'
 
 const completionChecker = async (user) => {
   if (user.wantsDevice && user.eligible && user.digiSkillsCompleted && user.courseRegistrationCompleted) {
