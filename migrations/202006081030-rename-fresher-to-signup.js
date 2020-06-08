ed1ed7d92ae2b1e439d5554c06dfabfadd937ee7
Tero Tapio <tero.tapio@helsinki.fi>
2020-06-08 10:43:04 +0300

    rename fresher_year to signup_year

--- migrations/202006081030-rename-fresher-to-signup.js
+++ migrations/202006081030-rename-fresher-to-signup.js
@@ -1,9 +1,9 @@
 module.exports = {
-    up(queryInterface) {
-      return queryInterface.renameColumn('users', 'fresher_year', 'signup_year')
-    },
+  up(queryInterface) {
+    return queryInterface.renameColumn('users', 'fresher_year', 'signup_year')
+  },
 
-    down(queryInterface) {
-      return queryInterface.renameColumn('users', 'signup_year', 'fresher_year')
-    },
-  }
+  down(queryInterface) {
+    return queryInterface.renameColumn('users', 'signup_year', 'fresher_year')
+  },
+}
