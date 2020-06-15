cfb067ba34e7baeb095183baf7ee1871ecc97237
Marko Koskinen <markokoskinen2037@gmail.com>
2020-06-15 13:48:04 +0300

    cron: close registrations when deadline is passed

--- server/util/cron.js
+++ server/util/cron.js
@@ -1,6 +1,20 @@
 const { CronJob } = require('cron')
 const logger = require('@util/logger')
 const { updateEligibleStudentStatuses, checkStudentEligibilities } = require('@services/student')
+const { getServiceStatusObject } = require('@controllers/serviceStatusController')
+
+const checkDeadline = async () => {
+  const ssObj = await getServiceStatusObject()
+  const { registrationDeadline, studentRegistrationOnline } = ssObj
+
+  if (studentRegistrationOnline) return
+
+  if (new Date().getTime() > new Date(registrationDeadline).getTime()) {
+    ssObj.studentRegistrationOnline = false
+    await ssObj.save()
+    logger.info('Deadline has passed. Registrations have been closed automatically.')
+  }
+}
 
 const startCron = () => new CronJob({
   cronTime: '0 * * * *',
@@ -17,6 +31,13 @@ const startCron = () => new CronJob({
       await checkStudentEligibilities()
     } catch (e) {
       logger.error('Failed checking student eligibilities!', e)
+    }
+
+    logger.info('Checking deadline...')
+    try {
+      await checkDeadline()
+    } catch (e) {
+      logger.error('Failed to check deadline', e)
     }
   },
   start: true,
