f6263ca4d94294108a9182ff12af465e536ac247
Marko Koskinen <markokoskinen2037@gmail.com>
2020-06-17 19:07:46 +0300

    add route for creating a lot of users for testing

--- server/controllers/testController.js
+++ server/controllers/testController.js
@@ -51,8 +51,60 @@ const resetServiceStatus = async (req, res) => {
   }
 }
 
+const createNewUser = async (i) => {
+  const b = Math.random() < 0.5
+  const c = Math.random() < 0.5
+
+  const defaults = {
+    userId: i,
+    studentNumber: b ? i : null,
+    name: i,
+    hyEmail: i,
+    personalEmail: b ? i : null,
+    distributor: !b,
+    staff: !b,
+    reclaimer: !b,
+    dateOfBirth: new Date('1.1.2050').getTime(),
+    device_distributed_by: b ? 'admin' : null,
+    deviceSerial: b ? `RAs${i}` : null,
+    deviceGivenAt: b ? new Date().getTime() : null,
+    eligible: b && c,
+    wantsDevice: b && c,
+    digiSkillsCompleted: b && c,
+    courseRegistrationCompleted: b && c,
+    adminNote: b ? 'hello' : null,
+    signupYear: b ? 2019 : 2020,
+    eligibilityReasons: {},
+  }
+
+  db.user.findOrCreate({
+    where: { userId: `${i}` },
+    defaults,
+  }).then(([, created]) => {
+    if (created) {
+      logger.info(`Created user ${i}`)
+    }
+  })
+}
+
+const createSomeUsers = async (req, res) => {
+  try {
+    let i = 0
+    while (i++ < 1000) {
+      createNewUser(i)
+    }
+
+
+    return res.status(200).end()
+  } catch (e) {
+    logger.error('error', e)
+    return res.status(500).json({ error: 'error' })
+  }
+}
+
 module.exports = {
   resetTestUsers,
   resetServiceStatus,
   disableStudentRegs,
+  createSomeUsers,
 }
--- server/util/routes.js
+++ server/util/routes.js
@@ -23,6 +23,7 @@ if (!inProduction) {
   router.get('/test/reset/user', testController.resetTestUsers)
   router.get('/test/reset/serviceStatus', testController.resetServiceStatus)
   router.get('/test/disableStudentRegs', testController.disableStudentRegs)
+  router.get('/test/createSomeUsers', testController.createSomeUsers)
 }
 
 router.get('/serviceStatus', serviceStatusController.getServiceStatus) // Before authentication, because contains translations and does not contain any sensitive data.
