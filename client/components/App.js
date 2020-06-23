73b1d74205d5fc5c4195ba7035ebb58600c3ad8b
Darake <adamson.gert@hotmail.com>
2020-06-23 13:21:59 +0300

    localize no student number error

--- client/components/App.js
+++ client/components/App.js
@@ -14,6 +14,14 @@ import { inProduction } from 'Utilities/common'
 import 'react-datepicker/dist/react-datepicker.css'
 import { getServiceStatus } from 'Utilities/redux/serviceStatusReducer'
 import useTranslation from 'Utilities/useTranslation'
+import { localeSelector } from 'Utilities/redux/localeReducer'
+
+const translations = {
+  noStudentNumber: {
+    en: 'You have no student number yet. Please check back in a day or two.',
+    fi: 'Teillä ei ole vielä opiskelijanumeroa. Yrittäkää uudestaan päivän tai parin päästä.',
+  },
+}
 
 const App = () => {
   const [sidebarVisible, setSidebarVisible] = useState(false)
@@ -21,6 +29,7 @@ const App = () => {
   const serviceStatus = useSelector(state => state.serviceStatus)
   const dispatch = useDispatch()
   const translatedRegClosedMsg = useTranslation('registrationClosed')
+  const locale = useSelector(localeSelector)
 
   useEffect(() => {
     dispatch(getUserAction())
@@ -35,8 +44,7 @@ const App = () => {
   if (user.error) {
     const studentNumberMissing = user.errorName === 'studentnumber-missing'
 
-    const message = studentNumberMissing ? 'Your have no studentnumber yet. Please check back in a day or two.'
-      : translatedRegClosedMsg
+    const message = studentNumberMissing ? translations.noStudentNumber[locale] : translatedRegClosedMsg
 
     return (
       <Sidebar hide={handleSidebarHide} visible={sidebarVisible}>
