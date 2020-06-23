55f50207e10e8372d327897069cc6cd26249da89
Darake <adamson.gert@hotmail.com>
2020-06-23 13:10:15 +0300

    localize InstructionModal

--- client/components/StudentPage/InstructionModal.js
+++ client/components/StudentPage/InstructionModal.js
@@ -1,23 +1,33 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { Modal, Button } from 'semantic-ui-react'
+import { localeSelector } from 'Utilities/redux/localeReducer'
 import TranslatedMarkdown from 'Components/TranslatedMarkdown'
+
+const translations = {
+  iHaveRead: {
+    en: 'I have read and understood the instructions',
+    fi: 'Olen lukenut ja ymmärtänyt ohjeet',
+  },
+}
 
 const InstructionModal = ({
   open, handleAcceptTermsClick, handleClose,
-}) => (
-  <Modal open={open} onClose={handleClose}>
-    <Modal.Content>
-      <TranslatedMarkdown textKey="acceptableTerms" />
-    </Modal.Content>
-    <Modal.Actions>
-      <Button inverted color="green" onClick={handleAcceptTermsClick} data-cy="acceptTerms">
-        Hyväksyn nämä ehdot
-        <br />
-        <br />
-        I accept these terms
-      </Button>
-    </Modal.Actions>
-  </Modal>
-)
+}) => {
+  const locale = useSelector(localeSelector)
+
+  return (
+    <Modal open={open} onClose={handleClose}>
+      <Modal.Content>
+        <TranslatedMarkdown textKey="acceptableTerms" />
+      </Modal.Content>
+      <Modal.Actions>
+        <Button inverted color="green" onClick={handleAcceptTermsClick} data-cy="acceptTerms">
+          {translations.iHaveRead[locale]}
+        </Button>
+      </Modal.Actions>
+    </Modal>
+  )
+}
 
 export default InstructionModal
