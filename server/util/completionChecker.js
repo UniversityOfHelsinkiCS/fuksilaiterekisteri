f8737c2d0d14a8bc961bf44edc6fa31d954ff757
Marko Koskinen <markokoskinen2037@gmail.com>
2020-06-25 15:15:51 +0300

    re-enable completionChecker automail

--- server/util/completionChecker.js
+++ server/util/completionChecker.js
@@ -1,14 +1,10 @@
-/* eslint-disable no-unreachable */
 const sendEmail = require('@util/sendEmail')
 const logger = require('@util/logger')
 
 const completionChecker = async (user, email) => {
-  return // For release safety. Will be removed later.
-
   if (user.wantsDevice && user.eligible && user.digiSkillsCompleted && user.courseRegistrationCompleted) {
     if (process.env.EMAIL_ENABLED !== 'true') {
       logger.error('Email disabled, set EMAIL_ENABLED=true to enable.')
-      // eslint-disable-next-line no-useless-return
       return
     }
     const info = await sendEmail([user.hyEmail, user.personalEmail], email.subject, email.body, email.replyTo)
