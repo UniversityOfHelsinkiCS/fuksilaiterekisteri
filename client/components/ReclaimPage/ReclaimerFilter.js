018b1ef9c4aeb935583f4ab2eafbd0aeb8005efb
Marko Koskinen <markokoskinen2037@gmail.com>
2020-06-30 13:25:10 +0300

    ReclaimerTable: Allow filtering by cause

---
+++ client/components/ReclaimPage/ReclaimerFilter.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { Segment, Radio } from 'semantic-ui-react'
+
+export default function ReclaimerFilter({
+  filter, setFilter, totalCount, filteredCount,
+}) {
+  const handleChange = (e, { value }) => {
+    setFilter(value)
+  }
+
+  const options = [{
+    key: 'all',
+    name: 'All',
+  }, {
+    key: 'fresherYearCredits',
+    name: 'Less than 30 credits on fresher year',
+  }, {
+    key: 'notPresent',
+    name: 'Not present',
+  }, {
+    key: 'deviceReturnDeadlinePassed',
+    name: 'Device owned for over 5 years',
+  }]
+
+  const style = {
+    padding: '0em 1em',
+  }
+
+
+  return (
+    <Segment>
+      <span>{`Showing ${filteredCount}/${totalCount}`}</span>
+
+      {options.map(({ key, name }) => (
+        <Radio
+          key={key}
+          data-cy={`${key}-filter`}
+          style={style}
+          label={name}
+          name="radioGroup"
+          value={key}
+          checked={filter === key}
+          onChange={handleChange}
+        />
+      ))}
+    </Segment>
+  )
+}
--- client/components/ReclaimPage/index.js
+++ client/components/ReclaimPage/index.js
@@ -1,11 +1,13 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState, useMemo } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Button } from 'semantic-ui-react'
 import { getStudentsWithOpenReclaimStatus, updateStudentReclainmStatuses } from 'Utilities/redux/studentReducer'
 import ReclaimTable from './ReclaimTable'
+import ReclaimerFilter from './ReclaimerFilter'
 
 const ReclaimPage = () => {
   const dispatch = useDispatch()
+  const [filter, setFilter] = useState('all')
 
   const students = useSelector(state => state.student.students)
 
@@ -17,12 +19,37 @@ const ReclaimPage = () => {
     dispatch(updateStudentReclainmStatuses())
   }
 
+  const doFiltering = () => {
+    let filtered
+    switch (filter) {
+      case 'all':
+        filtered = students
+        break
+      case 'fresherYearCredits':
+        filtered = students.filter(u => u.firstYearCredits < 30)
+        break
+      case 'notPresent':
+        filtered = students.filter(u => !u.present)
+        break
+      case 'deviceReturnDeadlinePassed':
+        filtered = students.filter(u => u.deviceReturnDeadlinePassed)
+        break
+      default:
+        filtered = students
+        break
+    }
+    return { filteredStudents: filtered }
+  }
+
+  const { filteredStudents } = useMemo(doFiltering, [filter, students])
+
   return (
     <div style={{ maxWidth: '100%', display: 'flex', flexDirection: 'column' }} data-cy="reclaimerContent">
       <Button onClick={handleUpdateClick} style={{ marginBottom: '1em' }} data-cy="updateReclaimStatuses">
         Run student status updater
       </Button>
-      <ReclaimTable students={students} />
+      <ReclaimerFilter filter={filter} setFilter={setFilter} totalCount={students.length} filteredCount={filteredStudents.length} />
+      <ReclaimTable students={filteredStudents} />
     </div>
   )
 }
