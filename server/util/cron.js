48b9fe95fd4cbeb8d26a58eb20f380c42e66f203
Marko Koskinen <markokoskinen2037@gmail.com>
2020-07-01 11:51:02 +0300

    Created cronjob for updating reclaimStatuses once per year

--- server/util/cron.js
+++ server/util/cron.js
@@ -1,6 +1,6 @@
 const { CronJob } = require('cron')
 const logger = require('@util/logger')
-const { updateEligibleStudentStatuses, checkStudentEligibilities } = require('@services/student')
+const { updateEligibleStudentStatuses, checkStudentEligibilities, updateStudentReclaimStatuses } = require('@services/student')
 const { getServiceStatusObject } = require('@controllers/serviceStatusController')
 
 const checkDeadline = async () => {
@@ -16,33 +16,53 @@ const checkDeadline = async () => {
   }
 }
 
-const startCron = () => new CronJob({
-  cronTime: '0 * * * *',
-  onTick: async () => {
-    logger.info('Updating eligible student statuses...')
-    try {
-      await updateEligibleStudentStatuses()
-    } catch (e) {
-      logger.error('Failed updating eligible student statuses!', e)
-    }
+const startCron = () => {
+  logger.info('Starting cron jobs')
+  /* eslint-disable no-new */
 
-    logger.info('Checking student eligibilities...')
-    try {
-      await checkStudentEligibilities()
-    } catch (e) {
-      logger.error('Failed checking student eligibilities!', e)
-    }
+  new CronJob({
+    cronTime: '0 * * * *',
+    onTick: async () => {
+      logger.info('Updating eligible student statuses...')
+      try {
+        await updateEligibleStudentStatuses()
+      } catch (e) {
+        logger.error('Failed updating eligible student statuses!', e)
+      }
 
-    logger.info('Checking deadline...')
-    try {
-      await checkDeadline()
-    } catch (e) {
-      logger.error('Failed to check deadline', e)
-    }
-  },
-  start: true,
-  timeZone: 'Europe/Helsinki',
-})
+      logger.info('Checking student eligibilities...')
+      try {
+        await checkStudentEligibilities()
+      } catch (e) {
+        logger.error('Failed checking student eligibilities!', e)
+      }
+
+      logger.info('Checking deadline...')
+      try {
+        await checkDeadline()
+      } catch (e) {
+        logger.error('Failed to check deadline', e)
+      }
+    },
+    start: true,
+    timeZone: 'Europe/Helsinki',
+  })
+
+  new CronJob({
+    cronTime: '30 0 15 9 *', // 15.9. at 00:30 once every year
+    onTick: async () => {
+      logger.info('Updating reclaim statuses...')
+      try {
+        await updateStudentReclaimStatuses()
+      } catch (e) {
+        logger.error('Failed updating reclaim statuses!', e)
+      }
+      logger.info('Reclaim statuses updated.')
+    },
+    start: true,
+    timeZone: 'Europe/Helsinki',
+  })
+}
 
 module.exports = {
   startCron,
