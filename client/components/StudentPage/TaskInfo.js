512cdeee035bdec33624b2fb750bffa2d5fa8f54
Marko Koskinen <markokoskinen2037@gmail.com>
2020-07-28 12:46:27 +0300

    Make TaskInfo messages more clear and prominent

--- client/components/StudentPage/TaskInfo.js
+++ client/components/StudentPage/TaskInfo.js
@@ -1,22 +1,21 @@
 import React from 'react'
-import { Segment } from 'semantic-ui-react'
+import { Message } from 'semantic-ui-react'
+import ReactMarkdown from 'react-markdown'
 import { useSelector } from 'react-redux'
 import { localeSelector } from 'Utilities/redux/localeReducer'
 
 const translations = {
   notRegisteredToCourse: {
-    en: 'You have not yet registered to a course on your study programme. '
+    en: 'You have not yet registered to a course on your study programme.\n\n'
       + 'Please plan your studies and register for courses in Weboodi. (The registration status is updated hourly.)',
-    fi: 'Et ole vielä ilmoittautunut oman opintolinjasi kurssille. Suunnittele '
-      + 'opintosi ja käy ilmoittautumassa kursseille Weboodissa. (Tämä tieto päivittyy tunneittain.)',
+    fi: 'Et ole vielä ilmoittautunut oman opintolinjasi kurssille. \n\n'
+      + 'Suunnittele opintosi ja käy ilmoittautumassa kursseille Weboodissa. (Tämä tieto päivittyy tunneittain.)',
   },
   digiSkillsNotCompleted: {
-    en: 'You are yet to complete the course Student\'s digital skills: '
-      + 'Orientation (DIGI-A). (Please note that this course is graded '
-      + 'only after its test period ends, so there may be a long delay before the '
-      + 'credit is registered.)',
-    fi: 'Et ole vielä suorittanut Opiskelijan digitaidot: orientaatio-kurssia (DIGI-A). '
-      + '(Kurssin suoritukset kirjataan noin 1-2 viikkoa koejakson jälkeen, jonka jälkeen suoritus näkyy myös täällä.)',
+    en: 'You are yet to complete the course Student\'s digital skills: Orientation (DIGI-A).\n\n'
+      + '(The course is graded and registered on the week following the examination period, after which the completion will also be displayed here.)',
+    fi: 'Et ole vielä suorittanut Opiskelijan digitaidot: orientaatio-kurssia (DIGI-A).\n\n'
+      + '(Kurssin suoritukset kirjataan koejakson jälkeisellä viikolla, jonka jälkeen suoritus näkyy myös täällä.)',
   },
 }
 
@@ -25,10 +24,10 @@ const TaskInfo = ({ courseRegistrationCompleted, digiSkillsCompleted }) => {
 
   if (courseRegistrationCompleted && digiSkillsCompleted) return null
   return (
-    <Segment.Group>
-      {!courseRegistrationCompleted && <Segment style={{ maxWidth: '500px' }}>{translations.notRegisteredToCourse[locale]}</Segment>}
-      {!digiSkillsCompleted && <Segment style={{ maxWidth: '500px' }}>{translations.digiSkillsNotCompleted[locale]}</Segment>}
-    </Segment.Group>
+    <div style={{ padding: '1em' }}>
+      {!courseRegistrationCompleted && <Message warning><ReactMarkdown source={translations.notRegisteredToCourse[locale]} /></Message>}
+      {!digiSkillsCompleted && <Message warning><ReactMarkdown source={translations.digiSkillsNotCompleted[locale]} /></Message>}
+    </div>
   )
 }
 
