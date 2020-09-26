91df8039a3ae04c835ef51db983f9db5ddf1af22
Darake <adamson.gert@hotmail.com>
2020-09-26 18:26:36 +0300

    check if distribution is over in competionChecker closes #317

--- server/util/completionChecker.js
+++ server/util/completionChecker.js
@@ -1,8 +1,14 @@
 const sendEmail = require('@util/sendEmail')
 const logger = require('@util/logger')
+const { ServiceStatus } = require('@models')
 
 const completionChecker = async (user, email) => {
   if (user.wantsDevice && user.eligible && user.digiSkillsCompleted && user.courseRegistrationCompleted) {
+    const settings = await ServiceStatus.getObject()
+    const isDistributionOver = new Date().getTime() > new Date(settings.taskDeadline).getTime()
+
+    if (isDistributionOver) return
+
     if (process.env.EMAIL_ENABLED !== 'true') {
       logger.error('Email disabled, set EMAIL_ENABLED=true to enable.')
       return
