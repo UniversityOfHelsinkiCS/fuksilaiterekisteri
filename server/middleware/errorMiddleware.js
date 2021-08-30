04bc89a2ae820f08e96f48b0d4c0f0e695bac3dc
Roni Lindholm <roni.lindholm@helsinki.fi>
2021-08-30 09:14:11 +0300

    Disable error middleware as making a mess to logs

--- server/middleware/errorMiddleware.js
+++ server/middleware/errorMiddleware.js
@@ -1,41 +1,24 @@
 /* eslint-disable prefer-rest-params */
 /* eslint-disable func-names */
-const Sentry = require('@sentry/node')
-const logger = require('@util/logger')
+// eslint-disable no-unused-vars
 
 const errorMiddleware = (req, res, next) => {
-  const oldWrite = res.write
-  const oldEnd = res.end
+  // const { originalUrl, method, query } = req
+  // const { statusCode } = res
 
-  const chunks = []
 
-  res.write = function (chunk) {
-    chunks.push(chunk)
-    return oldWrite.apply(res, arguments)
-  }
-
-  res.end = function (chunk) {
-    if (chunk) { chunks.push(chunk) }
-
-    const { statusCode } = res
-    if (statusCode >= 400 && req.headers.uid !== 'ohj_tosk') {
-      const body = JSON.parse(Buffer.concat(chunks).toString('utf8'))
-      const { originalUrl, method, query } = req
-      const errorMsg = body.error || ''
-      const message = `Response ${originalUrl} failed with status code ${statusCode} - ${errorMsg}`
-      logger.info({
-        originalUrl, body: JSON.stringify(body), method, query, message,
-      })
-      Sentry.withScope((scope) => {
-        scope.setUser(req.user ? req.user.get({ plain: true }) : null)
-        scope.setExtras({
-          originalUrl, body: JSON.stringify(body), method, query,
-        })
-        Sentry.captureMessage(message)
-      })
-    }
-    oldEnd.apply(res, arguments)
-  }
+  // if (statusCode >= 400 && req.headers.uid !== 'ohj_tosk') {
+  //   logger.info({
+  //     originalUrl, method, query, message: `${originalUrl} failed with status ${statusCode}`,
+  //   })
+  //   Sentry.withScope((scope) => {
+  //     scope.setUser(req.user ? req.user.get({ plain: true }) : null)
+  //     scope.setExtras({
+  //       originalUrl, method, query,
+  //     })
+  //     Sentry.captureMessage(`${originalUrl} failed with status ${statusCode}`)
+  //   })
+  // }
 
   next()
 }
