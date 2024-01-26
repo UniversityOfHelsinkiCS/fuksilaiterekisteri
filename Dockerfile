7a40e83a991d11543db448d6274a978bf8d8e0ff
Matti Luukkainen <mluukkai@iki.fi>
2024-01-26 13:50:34 +0200

    fix lint

--- Dockerfile
+++ Dockerfile
@@ -28,4 +28,5 @@ RUN npm run build
 
 EXPOSE 8000
 
+# CMD ["npm", "run", "start:dev"]
 CMD ["npm", "run", "start:prod"]
--- client/components/StudentPage/ExtendedTaskStatus.js
+++ client/components/StudentPage/ExtendedTaskStatus.js
@@ -1,78 +1,23 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
-import { Segment, Icon, Header } from 'semantic-ui-react'
-import { localeSelector } from 'Utilities/redux/localeReducer'
+import { Segment } from 'semantic-ui-react'
 import TranslatedMarkdown from 'Components/TranslatedMarkdown'
 import StudentInfo from './StudentInfo'
 // import TaskInfo from './TaskInfo'
 
-const translations = {
-  taskStatus: {
-    en: 'Task status:',
-    fi: 'Tehtävien tila:',
-  },
-  beFuksi: {
-    en: 'Be a fresher',
-    fi: 'Ole fuksi',
-  },
-  registeredToRelevant: {
-    en: 'Registered to relevant course',
-    fi: 'Rekisteröitynyt relevantille kurssille',
-  },
-  digiSkillsCompleted: {
-    en: 'DIGI-A completed',
-    fi: 'DIGI-A suoritettu',
-  },
-  tasksFinished: {
-    en: 'You have completed the tasks required for fresher device. Info about device distribution will be sent to you by email.',
-    fi: 'Olet suorittanut fuksilaitteeseen vaadittavat tehtävät. Saat tiedot laitteiden jakelusta sähköpostitse.',
-  },
-}
-
-const Task = ({ task, completed }) => {
-  if (completed) {
-    return (
-      <Segment style={{ background: '#d2f3db' }} textAlign="center">
-        {task}
-        <Icon name="checkmark" size="large" style={{ marginLeft: '10px' }} />
-      </Segment>
-    )
-  }
-  return (
-    <Segment style={{ background: '#fddede' }} textAlign="center">
-      {task}
-      <Icon name="cancel" size="large" style={{ marginLeft: '10px' }} />
-    </Segment>
-  )
-}
-
-const fake = {
-  user: {
-    eligible: true,
-    courseRegistrationCompleted: true,
-    digiSkillsCompleted: true,
-  },
-}
-
-const StudentStatusPage = ({ faking }) => {
-  const user = faking ? fake.user : useSelector(state => state.user.data)
-  const locale = useSelector(localeSelector)
-
-  return (
-    <Segment.Group>
-      <StudentInfo faking={faking} />
-      {/* <TaskInfo
+const StudentStatusPage = ({ faking }) => (
+  <Segment.Group>
+    <StudentInfo faking={faking} />
+    {/* <TaskInfo
         courseRegistrationCompleted={user.courseRegistrationCompleted}
         digiSkillsCompleted={user.digiSkillsCompleted}
       /> */}
-      <Segment>
+    <Segment>
         You will get an email when the device is ready!
-      </Segment>
-      <Segment>
-        <TranslatedMarkdown textKey="deviceSpecs" />
-      </Segment>
-    </Segment.Group>
-  )
-}
+    </Segment>
+    <Segment>
+      <TranslatedMarkdown textKey="deviceSpecs" />
+    </Segment>
+  </Segment.Group>
+)
 
 export default StudentStatusPage
