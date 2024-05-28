dc522e2e7b02deb1c2bf74afd79d7166ad8871a3
Matti Luukkainen <mluukkai@iki.fi>
2024-05-28 18:02:57 +0300

    try fix

--- client/components/VirtualizedTable/index.js
+++ client/components/VirtualizedTable/index.js
@@ -25,7 +25,7 @@ const VirtualizedTable = ({
     if (!selectedColumn || !(selectedColumn.getCellVal || selectedColumn.renderCell)) return data
     let result = lodashSortBy(data, selectedColumn.getCellVal || selectedColumn.renderCell)
 
-    if (process.env.NODE_ENV !== 'test' && sortBy === 'device_returned_at') {
+    if (false && sortBy === 'device_returned_at') {
       result = data.sort(dateBasedSorter)
     }
 
